76ce3cd825eb5e1100961d9889d96a31481e8831
Manish <manishvishwakarma324@gmail.com>
2025-03-03 09:04:19 +0530

    nothing changed

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { createRoot } from "react-dom/client";
+import React from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { SidebarProvider } from "./components/sidebar/sidebar_context/SidebarContext.tsx";
 import { DialogsProvider } from "./contexts/DialogsContext.tsx";
 import { MyListsProvider } from "./contexts/MyListsContext.tsx";
 
-createRoot(document.getElementById("root")!).render(
+React.createRoot(document.getElementById("root")!).render(
     <DialogsProvider>
       <MyListsProvider>
         <SidebarProvider>
