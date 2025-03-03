7dc4d2880c0eef7597542220f27a85daa4d304be
Manish <manishvishwakarma324@gmail.com>
2025-03-03 13:31:13 +0530

    nothing changed

--- src/components/sidebar/middle_section/Tags/TagsButtons.tsx
+++ src/components/sidebar/middle_section/Tags/TagsButtons.tsx
@@ -1,10 +1,8 @@
-import useDialogsContext from "../../../../contexts/useDialogsContext";
 import AddIconButton from "../../AddIconButton";
 import SelectTagColor from "./SelectTagColorButton";
 import TagsDropdownButton from "./TagsDropdownButton"
 
 const TagsButtons = () => {
-  const {toggleIsSelectTagColorInputOpen} = useDialogsContext();
   return (
     <div className="group flex-center-between section-heading">
       <TagsDropdownButton />
